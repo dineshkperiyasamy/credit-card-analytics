6af2a767620a2d9e929ae72e6d207bf2cb8ffc68
mtrdinesh <idinesh2k@gmail.com>
2021-01-04 19:14:40 -0700

    Unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,28 @@
+import 'zone.js/dist/zone-testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-
+import { CustomerDataService } from './services/customer-data.service';
+import { CustomerDataServiceMock } from './mock/customer-data-service.mock';
+import { BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+  import {MatMenuModule} from '@angular/material/menu';
+  
 describe('AppComponent', () => {
   beforeEach(async () => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatMenuModule
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        {provide: CustomerDataService, useClass: CustomerDataServiceMock}
+      ]
     }).compileComponents();
   });
 
@@ -20,16 +32,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'credit-card-analytics'`, () => {
+  it(`should have as title 'CUSTOMER ANALYTICS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('credit-card-analytics');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('credit-card-analytics app is running!');
+    expect(app.title).toEqual('CUSTOMER ANALYTICS');
   });
 });
